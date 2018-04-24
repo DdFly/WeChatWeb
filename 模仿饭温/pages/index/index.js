08c0ad44937b233170e334a524799eefd28185e6
DdFly <245145816@qq.com>
2018-04-25 01:33:29 +0800

    修改了饭温小程序，添加了tab，页面高度有bug

--- 模仿饭温/pages/index/index.js
+++ 模仿饭温/pages/index/index.js
@@ -11,17 +11,57 @@ Page({
       index_itemdata:{
           restaurant_name: '旭日苑',
           arrive_time: '12：00'
-      }
+      },
+      viewHidden1:"block",
+      currentTab: 0,
+      winWidth: 0,
+      winHeight: 0,
       
 
   },
 
 
+  bindChange: function (e) {
+
+      var that = this;
+      that.setData({ currentTab: e.detail.current });
+
+  },
+  /** 
+   * 点击tab切换 
+   */
+  tabclick: function (e) {
+
+      var that = this;
+
+      if (this.data.currentTab === e.target.dataset.current) {
+          return false;
+      } else {
+          that.setData({
+              currentTab: e.target.dataset.current
+          })
+      }
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+      var that = this;
+
+      /** 
+       * 获取系统信息 
+       */
+      wx.getSystemInfo({
+
+          success: function (res) {
+              that.setData({
+                  winWidth: res.windowWidth,
+                  winHeight: res.windowHeight
+              });
+          }
+
+      }); 
   },
 
   /**
